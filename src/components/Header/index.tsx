71f117be26a9302880038eb87eca72af9fc57e00
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2021-11-16 17:34:57 -0300

    instalação do sass; testando umas paradas com o header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,29 @@
+import styles from './Header.module.scss';
+
 const Header = () => {
   return (
-    <header>
-      <h2>Testando header</h2>
-      <p>tomara que esteja funcionando</p>
+    <header className={styles.header}>
+      <section className={styles.headerMain}>
+        <h2>Logozin</h2>
+
+        <div className={styles.searchContainer}>
+          <input type="text" placeholder="Busque aqui"/>
+        </div>
+
+        <div className={styles.accountInfo}>
+          Faça login ou cadastre-se
+        </div>
+
+        <div className={styles.iconsContainer}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </section>
+
+      <section className={styles.headerLinks}>
+        
+      </section>
     </header>
   )
 }
