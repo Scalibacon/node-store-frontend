7d5701fa8eeb166f3076cf6213b141fd2253bac1
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2021-11-26 16:56:36 -0300

    código da home migrado pro index pq o getserverprops não funcionava fora do top level da pages

--- src/pages/api/category.ts
+++ src/pages/api/category.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-type category = {
+export type category = {
   id: number,
   name: string;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,40 @@
-import type { NextPage } from 'next'
-import Home from '../pages/Home';
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
 
-const Index: NextPage = () => {
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { category } from './api/category';
+
+type HomeProps = {
+  categories?: category[],  
+}
+
+const Home: NextPage<HomeProps> = (props) => {
+  const [categories, setCategories] = useState(props.categories);
+
   return (
-    <Home/>
+    <>
+      <Header/>
+        <h3>Esse aqui é o mainzada</h3>
+        { 
+          categories?.map( (category, index) => {
+            return <div key={index}>{category.name}</div>
+          })
+        }
+      <Footer/>
+    </>    
   )
 }
 
-export default Index;
+export const getServerSideProps: GetServerSideProps = async () => {
+  const result = await fetch('http://localhost:3000/api/category');
+  const categories = await result.json();
+  
+  return {
+    props: {
+      categories,
+    }
+  }
+}
+
+export default Home;
